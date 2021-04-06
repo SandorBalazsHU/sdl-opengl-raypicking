019953dbc20f289b7a6527ecd5949e0cddc911f6
Sándor Balázs <it.sandor.balazs@gmail.com>
2021-04-06 17:33:24 +0200

    Raypicking basic code

---
+++ algorithm.cpp
@@ -0,0 +1,47 @@
+void Simulation::MouseDown(SDL_MouseButtonEvent& mouse) {
+	//Step 0: Get size
+	GLint m_viewport[4];
+	glGetIntegerv(GL_VIEWPORT, m_viewport);
+	float width = m_viewport[2];
+	float height = m_viewport[3];
+
+	//Step 1: 3d Normalised Device Coordinates
+	float x = (2.0f * mouse.x) / width - 1.0f;
+	float y = 1.0f - (2.0f * mouse.y) / height;
+	float z = 1.0f;
+	glm::vec3 ray_nds = glm::vec3(x, y, z);
+
+	//Step 2: 4d Homogeneous Clip Coordinates
+	glm::vec4 ray_clip = glm::vec4(ray_nds.x, ray_nds.y, -1.0, 1.0);
+
+	//Step 3: 4d Eye (Camera) Coordinates
+	glm::vec4 ray_eye = glm::inverse(camera.GetProj()) * ray_clip;
+	ray_eye = glm::vec4(ray_eye.x, ray_eye.y, -1.0, 0.0);
+
+	//Step 4: 4d World Coordinates
+	glm::vec3 ray_wor = glm::inverse(camera.GetViewMatrix()) * ray_eye;
+
+	//Step 5: Normalize
+	ray_wor = glm::normalize(ray_wor);
+
+	std::cout << "ray_wor: " << ray_wor.x << "," << ray_wor.y << "," << ray_wor.z << std::endl;
+
+	for (size_t i = 0; i < numberOfBalls; i++) {
+		glm::vec3 r0 = camera.GetEye();
+		glm::vec3 rd = ray_wor;
+		glm::vec3 s0 = positions[i];
+		float sr = 1.0f;
+
+		float a = glm::dot(rd, rd);
+		glm::vec3 s0_r0 = r0 - s0;
+		float b = 2.0 * glm::dot(rd, s0_r0);
+		float c = glm::dot(s0_r0, s0_r0) - (sr * sr);
+		float disc = b * b - 4.0 * a * c;
+		if (disc < 0.0) {
+			//std::cout << "ERROR" << std::endl;
+		}
+		else {
+			colors[i] = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+		}
+	}
+}
